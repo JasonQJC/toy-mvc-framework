1ad1e7d5c85dc11f5ec783469e8a7cd52205558b
JasonQJC <583917983@qq.com>
2021-04-17 04:05:37 +0800

    impl one servlet,change jstl impl dependency

--- src/main/java/com/jasonqjc/version1/controller/CoffeeShowServlet.java
+++ src/main/java/com/jasonqjc/version1/controller/CoffeeShowServlet.java
@@ -1,6 +1,10 @@
 package com.jasonqjc.version1.controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import com.jasonqjc.version1.model.Coffee;
+import com.jasonqjc.version1.service.CoffeeService;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,16 +15,18 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/coffee_show")
 public class CoffeeShowServlet extends HttpServlet {
 
+  private CoffeeService service;
+  
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    super.doGet(req, resp);
+  public void init() throws ServletException {
+    service = new CoffeeService();
   }
 
   @Override
-  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    // TODO Auto-generated method stub
-    super.doPost(req, resp);
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    List<Coffee> coffeeList = service.getCoffeeList();
+    req.setAttribute("coffeeList", coffeeList);
+    req.getRequestDispatcher("/view/coffee.jsp").forward(req, resp);
   }
 
 }
